92d4c16a53fab50bff013d5529978af9f5e53ee4
Kartar Jabanda <kartar.jabanda@remarkable.net>
2020-03-12 15:39:22 +0000

    start section 15 - redux store design

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
-export const fetchPosts = () => {
-  return {
-    type: "FETCH_POSTS"
-  };
-};
+import jsonplaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonplaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response})
+}
