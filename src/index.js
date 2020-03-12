588c681075b3e6056b4cf5d391935e65e0f213c3
Kartar Jabanda <kartar.jabanda@remarkable.net>
2020-03-12 11:07:59 +0000

    fix broken import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "react";
+import { createStore } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
